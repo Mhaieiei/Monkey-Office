974c8b5d15419c379a01fa9031328960fdec7f83
Makemek <makemek101@hotmail.com>
2016-01-30 15:43:15 +0700

    refactor routing homepage to it own file

--- webproject/routes/direct.js
+++ webproject/routes/direct.js
@@ -30,29 +30,7 @@ var nametemp = "";
 
 module.exports = function(app, passport) {
 
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
-    app.get('/', function(req, res) {
-        res.render('welcome.hbs',{layout:"main"});
-    });
-	
-	// Get path images
-	app.get('/image.png', function (req, res) {
-			res.sendfile(path.resolve('uploads/image_'+req.user._id+'.jpg'));
-	});
-	app.get('/imagelogo.jpg', function (req, res) {
-    		res.sendfile(path.resolve('public/images/monkey.jpg'));
-	});
-	app.get('/db.jpg', function (req, res) {
-    		res.sendfile(path.resolve('public/images/db.jpg'));
-	});
-	app.get('/qa.jpg', function (req, res) {
-    		res.sendfile(path.resolve('public/images/qa.jpg'));
-	});
-	app.get('/wf.jpg', function (req, res) {
-    		res.sendfile(path.resolve('public/images/wf.jpg'));
-	});
+	require('../controllers/common/home')(app);
 	
     // =====================================
     // LOGIN ===============================
