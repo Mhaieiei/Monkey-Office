993c0a7a24854225d2d0bfb5b1516a90040f2cee
Makemek <makemek101@hotmail.com>
2016-01-30 16:11:12 +0700

    refactor signup page routing

--- webproject/routes/direct.js
+++ webproject/routes/direct.js
@@ -32,22 +32,7 @@ module.exports = function(app, passport) {
 
 	require('../controllers/common/home')(app);
 	require('../controllers/authentication/auth')(app, passport);
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
-    app.get('/signup', function(req, res) {
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: 'signupMessage' });
-    });
-
-    // process the signup form
-    // app.post('/signup', do all our passport stuff here);
-	app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
+    require('../controllers/authentication/signup')(app, passport);
 	 // =====================================
     // HOME SECTION =====================
     // =====================================
