c6141de4030a8b1483637e098f0012dc53c5d835
Makemek <makemek101@hotmail.com>
2016-01-30 16:00:05 +0700

    refactor login logout page routing

--- webproject/routes/direct.js
+++ webproject/routes/direct.js
@@ -31,37 +31,7 @@ var nametemp = "";
 module.exports = function(app, passport) {
 
 	require('../controllers/common/home')(app);
-	
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    app.get('/login', function(req, res) {
-
-        // render the page and pass in any flash data if it exists     
-        res.render('login.ejs', { message: req.flash('loginMessage') }); 
-    });
-
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/home', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-		
-		
-    }));
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
-    app.get('/logout', function(req, res) {
-		console.log("Get logout");
-        req.logout();
-        res.redirect('/');
-    });
-	
-
-
+	require('../controllers/authentication/auth')(app, passport);
     // =====================================
     // SIGNUP ==============================
     // =====================================
