b7779b67433b01dc0d814be7b43f089ec9998502
Makemek <makemek101@hotmail.com>
2016-03-03 19:54:37 +0900

    [refactor schemaLoader.js] remove repetitions when loading database schemas

--- webnew/utility/schemaLoader.js
+++ webnew/utility/schemaLoader.js
@@ -1,30 +1,17 @@
 module.exports = function(database) {
 
-	database.model('User', require('../model/user'));
-	database.model('Work', require('../model/works'));
-	database.model('Document', require('../model/document'));
+	var models = {};
 
-	database.model('Faculty', require('../model/faculty'));
-	database.model('Subject', require('../model/subject'));
-	database.model('AcademicYear', require('../model/academic_year'));
-	database.model('TeachingSemester', require('../model/teaching_semester'));
-	database.model('TemplateWorkflow', require('../model/TemplateWorkflow'));
-	database.model('Document', require('../model/document'));
-	database.model('Stdenroll', require('../model/student_enroll'));
-	database.model('Subenroll', require('../model/subject_enroll'));
-
-	var models = {
-		User: database.model('User'),
-		Work: database.model('Work'),
-		Document: database.model('Document'),
-		Faculty: database.model('Faculty'),
-		Subject: database.model('Subject'),
-		AcademicYear: database.model('AcademicYear'),
-		TeachingSemester: database.model('TeachingSemester'),
-		TemplateWorkflow: database.model('TemplateWorkflow'),
-		StudentEnroll: database.model('Stdenroll'),
-		SubjectEnroll: database.model('Subenroll')
-	}
+	models.User             = database.model('User', require('../model/user'));
+	models.Work             = database.model('Work', require('../model/works'));
+	models.Document         = database.model('Document', require('../model/document'));
+	models.Faculty          = database.model('Faculty', require('../model/faculty'));
+	models.Subject          = database.model('Subject', require('../model/subject'));
+	models.AcademicYear     = database.model('AcademicYear', require('../model/academic_year'));
+	models.TeachingSemester = database.model('TeachingSemester', require('../model/teaching_semester'));
+	models.TemplateWorkflow = database.model('TemplateWorkflow', require('../model/TemplateWorkflow'));
+	models.StudentEnroll    = database.model('Stdenroll', require('../model/student_enroll'));
+	models.SubjectEnroll    = database.model('Subenroll', require('../model/subject_enroll'));
 
 	return models;
 }
