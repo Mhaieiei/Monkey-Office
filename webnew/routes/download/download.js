82644930383d38fb57303866aad3eb6617043e7d
Makemek <makemek101@hotmail.com>
2016-03-22 03:42:15 +0900

    check if user owns the destinated file

--- webnew/routes/download/download.js
+++ webnew/routes/download/download.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var schemas = require('utility/schemaLoader')(require('lib/dmsDb'));
 
 router.get('/document/:file', isGranted, function(req, res) {
 	var file = path.resolve('uploads/document/' + req.params.file);
@@ -11,14 +12,40 @@ router.get('/document/:file', isGranted, function(req, res) {
 /*
 Check whether user have permission to download a file
 	- User must have already logged in to the system
-	- User must own that document or the document can be downloaded by any members
+	- User must own that file or the file can be downloaded by any members
 */
 function isGranted(req, res, next) {
 	var isLoggedIn = require('middleware/loginChecker');
 	isLoggedIn(req, res, next);
-
-	
+	isMyFile(req, res, next);
 }
 
+/*
+Check whether user owns the destinated file
+If user doesn't own the file, redirect to root
+*/
+function isMyFile(req, res, next) {
+	var user = req.user;
+	var filename = req.params.file;
+
+	var query = schemas.Document.findOne({
+		'personReceive': user,
+		'name': filename
+	});
+
+	query.exec(function(err, _docs) {
+		if(err) {
+			var err = new Error(err);
+			err.status = 500;
+			return next(err);
+		}
+
+		console.log(_docs);
+		if(!_docs)
+			res.redirect('/home');
+		else
+			return next();
+	});
+}
 
 module.exports = router;
