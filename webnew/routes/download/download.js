81bf2c24ad6f5027a20ae501a83b57af727f6d85
Makemek <makemek101@hotmail.com>
2016-03-22 01:01:25 +0900

    allow only loggedin user to download a document

--- webnew/routes/download/download.js
+++ webnew/routes/download/download.js
@@ -2,10 +2,23 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-router.get('/document/:file', function(req, res) {
+router.get('/document/:file', isGranted, function(req, res) {
 	var file = path.resolve('uploads/document/' + req.params.file);
 	console.log('request file: ' + file);
 	res.download(file);
 });
+
+/*
+Check whether user have permission to download a file
+	- User must have already logged in to the system
+	- User must own that document or the document can be downloaded by any members
+*/
+function isGranted(req, res, next) {
+	var isLoggedIn = require('middleware/loginChecker');
+	isLoggedIn(req, res, next);
+
+	
+}
+
 
 module.exports = router;
