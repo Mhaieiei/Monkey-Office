751baa8c2f17377bffac5f8732e961272a09a2f2
Makemek <makemek101@hotmail.com>
2016-03-22 00:43:57 +0900

    move isloggedin() to loginchecker.js

--- webnew/routes/direct.js
+++ webnew/routes/direct.js
@@ -3539,16 +3539,6 @@ module.exports = function(app, passport, schemas) {
 
 };
 
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
 //route middleware to make sure user is logged in as Admin
 function isAdmin(req,res,next){
 
