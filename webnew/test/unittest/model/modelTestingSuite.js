c687453443141226fd4f920cfb028121d37d4f16
Makemek <makemek101@hotmail.com>
2016-04-04 05:17:56 +0900

    combine testsuite 'Document Model' and 'Document Sub Type' to one testsuite

--- webnew/test/unittest/model/modelTestingSuite.js
+++ webnew/test/unittest/model/modelTestingSuite.js
@@ -9,8 +9,10 @@ describe('Database Collections Entity Testing', function() {
 		var app = require('app')(dbMock);
 	})
 
-	makeSuite('Document Model', require('./testDocumentModel'));
-	makeSuite('Document Sub Type', require('./document/department/testDocumentSubType'));
+	makeSuite('Document Model', function() {
+		describe('Base Schema', require('./testDocumentModel'));
+		describe('Document Sub Type', require('./document/department/testDocumentSubType'));
+	});
 })
 
 function makeSuite(name, tests) {
