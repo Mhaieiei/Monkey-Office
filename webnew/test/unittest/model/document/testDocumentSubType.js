50b53dcb6fa3b44f1f37a2004571ecc2f359d76d
Makemek <makemek101@hotmail.com>
2016-04-03 22:44:09 +0900

    Add testcase

---
+++ webnew/test/unittest/model/document/testDocumentSubType.js
@@ -0,0 +1,26 @@
+var rootpath = require('rootpath')();
+var expect = require('chai').expect;
+const assert = require('assert');
+
+var dbMock = require('test/dbTestConfig');
+var app = require('app')(dbMock);
+
+describe('Document Sub Type', function() {
+
+	var doc1;
+
+	before(function(done) {
+		AcademicAdminDoc = require('model/document/department/academic/academicAdministration');
+		doc1 = new AcademicAdminDoc({
+			name: 'AcademicAdminDoc'
+		});
+		doc1.save(function(err) {
+			assert.ifError(err);
+			done();
+		});
+	})
+
+	it('documentID should start at 1', function() {
+		expect(doc1.docNum).to.equals(1);
+	});
+})
