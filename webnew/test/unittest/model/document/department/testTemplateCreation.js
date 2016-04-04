fd9dbb907aca20634cf45b4bef723edb649217c3
Makemek <makemek101@hotmail.com>
2016-04-05 03:56:26 +0900

    implement testcase 'should include additional field if defined'

--- webnew/test/unittest/model/document/department/testTemplateCreation.js
+++ webnew/test/unittest/model/document/department/testTemplateCreation.js
@@ -12,8 +12,21 @@ module.exports = function() {
 		expect(xx.department).to.equals(type);
 	});
 
-	it("should return the same schema if additional key-value pairs don't add", function() {
-		throw "Not implemented";
+	it("should include additional field if defined", function() {
+		var type = 'XY';
+		var additionalFields = {field1: Number, field2: Number, field3: Number};
+		var additionalFieldsValue = {field1: 1, field2: 2, field3: 3};
+		var Doc = template(type, additionalFields);
+		var DocWithAdditionalFields = new Doc(additionalFieldsValue);
+
+		DocWithAdditionalFields.save(function(err) {
+			expect(DocWithAdditionalFields.field1).to.exist;
+			expect(DocWithAdditionalFields.field1).to.equal(additionalFieldsValue.field1);
+			expect(DocWithAdditionalFields.field2).to.exist;
+			expect(DocWithAdditionalFields.field2).to.equal(additionalFieldsValue.field2);
+			expect(DocWithAdditionalFields.field3).to.exist;
+			expect(DocWithAdditionalFields.field3).to.equal(additionalFieldsValue.field3);
+		})
 	});
 
 	describe('Parameterize test on template parameters', function() {
